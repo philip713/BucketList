39652ff9b1fc222c336640a063b7954f9f58d995
philip713 <105677806+philip713@users.noreply.github.com>
2025-07-28 05:14:41 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Philip Janzel Paradeza on 2025-07-28.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
