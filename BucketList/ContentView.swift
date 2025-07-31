249a1fe1866df299047246209802195dfe39577d
philip713 <105677806+philip713@users.noreply.github.com>
2025-07-30 21:44:14 -0600

    mvvm

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,18 +6,63 @@
 //
 
 import SwiftUI
-
+import MapKit
+import LocalAuthentication
 struct ContentView: View {
+   let  startPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 56, longitude: -3), span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)))
+    @State private var viewModel = ViewModel()
+    @State private var isUnlocked = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader{ proxy in
+            Map(initialPosition: startPosition){
+                ForEach(viewModel.locations){ location in
+                    Annotation(location.name, coordinate: location.coordinate){
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture {
+                                viewModel.selectedPlace = location
+                            }
+                    }
+                }
+            }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local){
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace){ place in
+                    Text(place.name)
+                    EditView(location: place){
+                        viewModel.update(location: $0)
+                    }
+                }
         }
-        .padding()
+    }
+    
+    func authenticate(){
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error){
+            let reason = "We need to unlock your data"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authenticationError in
+                if success{
+                    isUnlocked = true
+                } else {
+                    //problem
+                }
+            }
+        } else {
+            //no biometrics
+        }
     }
 }
+
+
 
 #Preview {
     ContentView()
--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -4,5 +4,19 @@
 //
 //  Created by Philip Janzel Paradeza on 2025-07-28.
 //
-
 import Foundation
+import MapKit
+struct Location: Codable, Equatable, Identifiable {
+    var id: UUID
+    var name: String
+    var description: String
+    var latitude: Double
+    var longitude: Double
+    var coordinate: CLLocationCoordinate2D{
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    static let example = Location(id: UUID(), name: "buckingham", description: "lit by 40000 lightbukbs", latitude: 51.501, longitude: -0.141)
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
